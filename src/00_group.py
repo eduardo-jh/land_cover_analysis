9015787f5c40a275c31e6e0d55ef8c9b28e66926
Eduardo Jimenez (VIP Lab) <eduardojh@arizona.edu>
2023-05-12 15:23:49 -0700

    Group land cover classes

---
+++ src/00_group.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+""" Reclasify and merge land cover classes into groups, which are more easy to work with.
+
+NOTICE: run from 'rsml' environment (Python 3.8.13; GDAL 3.5.2 & matplotlib 3.6.0 from conda-forge)
+
+author: Eduardo Jimenez <eduardojh@arizona.edu>
+Changelog:
+    May 12, 2023: Initial code.
+"""
+
+import sys
+import platform
+import csv
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+NA_CLASS = 0
+
+# adding the directory with modules
+system = platform.system()
+if system == 'Windows':
+    # On Windows 10
+    sys.path.insert(0, 'D:/Desktop/land_cover_analysis/lib/')
+    cwd = 'D:/Desktop/CALAKMUL/ROI1/'
+# elif system == 'Linux':
+#     # On Alma Linux Server
+#     sys.path.insert(0, '/home/eduardojh/Documents/land_cover_analysis/lib/')
+#     cwd = '/VIP/anga/DATA/USGS/LANDSAT/DOWLOADED_DATA/AutoEduardo/DATA/CALAKMUL/ROI1/'
+elif system == 'Linux':
+    # On Ubuntu Workstation
+    sys.path.insert(0, '/vipdata/2023/land_cover_analysis/lib/')
+    cwd = '/vipdata/2023/CALAKMUL/ROI1/'
+else:
+    print('System not yet configured!')
+
+import rsmodule as rs
+
+fn_landcover = cwd + 'training/usv250s7cw_ROI1_LC_KEY.tif'
+fn_keys = cwd + 'training/usv250s7cw_ROI1_updated.txt'
+fn_stats = cwd + 'training/usv250s7cw_ROI1_statistics.csv'
+fn_testing_mask  = cwd + 'training/usv250s7cw_ROI1_testing_mask.tif'  # create testing mask, training is the complement
+
+inegi_indices = (2, 1, 4)  # INEGI's land cover key, desciption, and group
+lc_ind, lc_grp = rs.land_cover_freq(fn_landcover, fn_keys, inegi_indices, verbose=False)
+
+# Print land cover frequencies
+keys = sorted(lc_ind.keys())
+print("\nFrequency for individual land cover classes.")
+print(f"{'Key':>8} {'Frequency':>12}")
+for key in keys:
+    print(f'{key:>8} {lc_ind[key]:>12}')
+
+keys = sorted(lc_grp.keys())
+print("\nFrequency for group of land cover classes.")
+print(f"{'Key':>8} {'Frequency':>12}")
+for key in keys:
+    print(f'{key:>8} {lc_grp[key]:>12}')
+
+# Get the land cover classes by group
+lc_grp = rs.land_cover_by_group(fn_landcover, fn_keys, inegi_indices)
+print(lc_grp)
