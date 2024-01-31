4a224ff312a7abe7df92222c154a2138b4f8fde1
eduardojh <eduardojh@arizona.edu>
2024-01-30 17:30:45 -0700

    Fixed pixel count comparison, barplot, and report

--- src/validation.py
+++ src/validation.py
@@ -19,16 +19,16 @@ fmt = '%Y_%m_%d-%H_%M_%S'
 
 def validation(cwd: str, fn_predictions: str, fn_validation: str, **kwargs):
 
-    _prefix = kwargs.get('prefix', '')
+    _prefix = kwargs.get('prefix', '1x1')
     exec_start = datetime.now()
 
-    fn_save_raster = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_preds_val_sites.tif')
-    fn_save_report = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_validation_report.txt')
-    fn_save_conf_matrix = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_validation_conf_matrix.csv')
-    fn_save_conf_fig_pa = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_validation_conf_matrix_pa.png')
-    fn_save_conf_fig_ua = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_validation_conf_matrix_ua.png')
-    fn_save_params = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_exec_params.csv')
-    fn_save_bars = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_barplot.png')
+    fn_save_raster = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_preds_sel_sites.tif')
+    fn_save_report = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_validation_report.txt')
+    fn_save_conf_matrix = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_validation_conf_matrix.csv')
+    fn_save_conf_fig_pa = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_validation_conf_matrix_pa.png')
+    fn_save_conf_fig_ua = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_validation_conf_matrix_ua.png')
+    fn_save_params = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_exec_params.csv')
+    fn_save_bars = os.path.join(cwd, 'validation', f'{datetime.strftime(exec_start, fmt)}_{_prefix}_barplot.png')
 
     # Read the land cover raster and retrive the land cover classes
     assert os.path.isfile(fn_predictions) is True, f"ERROR: File not found! {fn_predictions}"
@@ -51,11 +51,6 @@ def validation(cwd: str, fn_predictions: str, fn_validation: str, **kwargs):
     print(f'    --Spatial ref.  : {valid_ref}')
     print(f'    --Type          : {valid_arr.dtype}')
 
-    # print("Values from the land cover predictions:")
-    # print(np.unique(pred_arr, return_counts=True))
-    # print("Values from the validation dataset:")
-    # print(np.unique(valid_arr, return_counts=True))
-
     # Get the predictions where there are validation sites
     select_arr = np.where(valid_arr > 0, pred_arr, 0)
     print(f"Saving raster of predictions to match places with validation points: {fn_save_raster}")
@@ -69,8 +64,11 @@ def validation(cwd: str, fn_predictions: str, fn_validation: str, **kwargs):
     pred_comp = pred_arr[mask]
     valid_comp = valid_arr[mask]
 
-    print(f"Non-zero values: pred={pred_comp.sum()} valid={valid_comp.sum()} diff={abs(valid_comp.sum()-pred_comp.sum())}")
+    # print(f"Non-zero values: pred={(pred_arr>0).sum()} valid={(valid_arr>0).sum()} diff={abs((valid_arr>0).sum()-(pred_arr>0).sum())}")
+    print(f"Non-zero values: pred={(pred_comp>0).sum()} valid={(valid_comp>0).sum()} diff={abs((valid_comp>0).sum()-(pred_comp>0).sum())}")
+    print(f"Sum of values: pred={pred_comp.sum()} valid={valid_comp.sum()} diff={abs(valid_comp.sum()-pred_comp.sum())}")
 
+    # Get the unique class values and their pixel count
     pred_cls, pred_counts = np.unique(pred_comp, return_counts=True)
     valid_cls, valid_counts = np.unique(valid_comp, return_counts=True)
     print("Values from the land cover predictions:")
@@ -79,38 +77,56 @@ def validation(cwd: str, fn_predictions: str, fn_validation: str, **kwargs):
     print(valid_cls, valid_counts)
 
     # Create a dataset to match the label's classes
-    y2 = np.zeros(pred_cls.shape, dtype=np.int16)
-    j = 0
-    for i, cls in enumerate(pred_cls):
+    common_cls = np.unique(np.concatenate((pred_cls, valid_cls)))
+
+    class_names = [str(x) for x in common_cls]
+    print(f"Class names for cross-tabulation: {class_names}")
+
+    y1 = np.zeros(common_cls.shape, dtype=np.int16)
+    y2 = np.zeros(common_cls.shape, dtype=np.int16)
+    
+    # Match the pixel counts using a common classes array
+    j = 0  # counter for valid_counts
+    k = 0  # counter for pred_counts
+    for i, cls in enumerate(common_cls):
+        if cls in pred_cls:
+            y1[i] = pred_counts[k]
+            k += 1
         if cls in valid_cls:
             y2[i] = valid_counts[j]
             j += 1
-    # Remove the first value if it's zero
+    
+    # Adjust for plotting if the first value is zero
+    common_cls_plt = common_cls.copy()
     if pred_cls[0] == 0:
-        print("WARNING: removing the first value of each list.")
-        pred_cls = pred_cls[1:]
-        pred_counts = pred_counts[1:]
-        y2 = y2[1:]
-    print(pred_cls)
-    print(pred_counts)
-    print(y2)
+        common_cls_plt = np.arange(0, len(common_cls))
+        print(f"Adjusting x-axis to: {common_cls_plt}")
+    print(f"Common classes: {common_cls}")
+    print(f"Pred  counts 1: {y1}")
+    print(f"Valis counts 2: {y2}")
 
     # Plot the label counts
     width = 0.4
-    fig = plt.figure(figsize=(14,12))
-    plt.bar(pred_cls-width, pred_counts, width, label="Predictions", log=True)
-    plt.bar(pred_cls, y2, width, label="Validation", log=True)
-    # plt.bar(pred_cls-width, pred_counts, width, label="Predictions",)
-    # plt.bar(pred_cls, y2, width, label="Validation")
-    plt.xlabel("Land cover classes")
-    plt.ylabel("Pixel count")
-    plt.xticks(np.arange(101, 112))
-    plt.legend(loc='best')
+    # fig = plt.figure(figsize=(14,12))
+    # plt.bar(pred_cls-width, pred_counts, width, label="Predictions", log=True)
+    # plt.bar(pred_cls, y2, width, label="Validation", log=True)
+    # # plt.bar(pred_cls-width, pred_counts, width, label="Predictions",)
+    # # plt.bar(pred_cls, y2, width, label="Validation")
+    # plt.xlabel("Land cover classes")
+    # plt.ylabel("Pixel count")
+    # plt.xticks(np.arange(101, 112))
+    # plt.legend(loc='best')
+    
+    fig, ax = plt.subplots(layout='constrained')
+    r1 = ax.bar(common_cls_plt-width, y1, width, label="Predictions", log=True)
+    r2 = ax.bar(common_cls_plt, y2, width, label="Validation", log=True)
+    ax.bar_label(r1, padding=3, fontsize=6)
+    ax.bar_label(r2, padding=3, fontsize=6)
+    ax.set_xlabel("Land cover classes")
+    ax.set_ylabel("Pixel count")
+    ax.set_xticks(common_cls_plt, class_names)
+    ax.legend(loc='upper left', ncol=2)
     plt.savefig(fn_save_bars, bbox_inches='tight', dpi=300)
-
-    class_names_ = np.unique(pred_comp)
-    class_names = [str(x) for x in class_names_]
-    print(f"Class names for cross-tabulation: {class_names}")
 
     accuracy = accuracy_score(valid_comp, pred_comp)
     print(f'***Accuracy score: {accuracy:>0.4f}***')
@@ -176,10 +192,38 @@ def validation(cwd: str, fn_predictions: str, fn_validation: str, **kwargs):
     with open(fn_save_params, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(['Predictions file', fn_predictions])
+        writer.writerow(['  NoData', pred_nd])
+        writer.writerow(['  Rows', pred_arr.shape[0]])
+        writer.writerow(['  Columns', pred_arr.shape[1]])
+        writer.writerow(['  Geotransform', pred_gt])
+        writer.writerow(['  Spatial reference', pred_ref])
+        writer.writerow(['  Data type', pred_arr.dtype])
         writer.writerow(['Validation file', fn_validation])
-        writer.writerow(['Validation raster', fn_save_raster])
+        writer.writerow(['  NoData', valid_nd])
+        writer.writerow(['  Rows', valid_arr.shape[0]])
+        writer.writerow(['  Columns', valid_arr.shape[1]])
+        writer.writerow(['  Geotransform', valid_gt])
+        writer.writerow(['  Spatial reference', valid_ref])
+        writer.writerow(['  Data type', valid_arr.dtype])
+        writer.writerow(['Masked predictions raster', fn_save_raster])
+        writer.writerow(['Validation pixels (mask)', np.sum(mask)])
+        writer.writerow(['Non-zero prediction pixels (masked)', (pred_comp>0).sum()])
+        writer.writerow(['Non-zero validation pixels (masked)', (valid_comp>0).sum()])
+        writer.writerow(['Non-zero pixels difference', abs((valid_comp>0).sum()-(pred_comp>0).sum())])
+        writer.writerow(['Sum of predictions', pred_comp.sum()])
+        writer.writerow(['Sum of validation', valid_comp.sum()])
+        writer.writerow(['Sum difference', abs(valid_comp.sum()-pred_comp.sum())])
+        writer.writerow(['Prediction classes', ';'.join([str(x) for x in pred_cls])])
+        writer.writerow(['Prediction pixel count', ';'.join(str(x) for x in pred_counts)])
+        writer.writerow(['Validation classes', ';'.join(str(x) for x in valid_cls)])
+        writer.writerow(['Validation pixel count', ';'.join(str(x) for x in valid_counts)])
+        writer.writerow(['Common classes', ';'.join(str(x) for x in common_cls)])
+        writer.writerow(['Prediction pixel count (matched)', ';'.join(str(x) for x in y1)])
+        writer.writerow(['Validation pixel count (matched)', ';'.join(str(x) for x in y2)])
+        writer.writerow(['Class names', ';'.join(class_names)])
         writer.writerow(['Accuracy score', accuracy])
         writer.writerow(['Kappa', kappa])
+        writer.writerow(['Bar plot pixel count comparison', fn_save_bars])
         writer.writerow(['Confusion matrix file', fn_save_conf_matrix])
         writer.writerow(['Producer accuracy figure', fn_save_conf_fig_pa])
         writer.writerow(['User accuracy figure', fn_save_conf_fig_ua])
@@ -199,15 +243,29 @@ if __name__ == "__main__":
     # =============================== 2016-2019 ===============================
     cwd = '/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2016_2019/'
     fn_preds_map = os.path.join(cwd, 'results/2023_10_28-18_19_05', '2023_10_28-18_19_05_predictions.tif')
-    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_r105.tif')
-    fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_r105_2015_2016.tif')
+    
+    # Compare against the entire dataset (all years)
+    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts.tif')
+    # id="1x1"
+    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_r45_3x3.tif')
+    # id="3x3"
+    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_r75_5x5.tif')
+    # id="5x5"
+    
+    # Compare against the dataset for the matching period with this INEGI series VI (2015-2016)
+    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_2015_2016.tif')
+    # id="1x1p"
+    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_2015_2016_r45_3x3.tif')
+    # id="3x3p"
+    fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_2015_2016_r75_5x5.tif')
+    id="5x5p"
 
     # =============================== 2019-2022 ===============================
-    cwd = '/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2016_2019/'
-    fn_preds_map = os.path.join(cwd, 'results/2023_10_28-18_19_05', '2023_10_28-18_19_05_predictions.tif')
-    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_r105.tif')
-    fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts_r105_2015_2016.tif')
+    # cwd = '/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2016_2019/'
+    # fn_preds_map = os.path.join(cwd, 'results/2023_10_28-18_19_05', '2023_10_28-18_19_05_predictions.tif')
+    # # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts.tif')
+    # fn_valid_map = os.path.join(cwd, 'validation', 'infys_pts.tif')
 
     # Run validation
-    validation(cwd, fn_preds_map, fn_valid_map)
+    validation(cwd, fn_preds_map, fn_valid_map, prefix=id)
 
