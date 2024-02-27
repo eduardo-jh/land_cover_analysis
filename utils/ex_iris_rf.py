73c676670586c86465eb18d504b101d11c1bcbba
eduardojh <eduardojh@arizona.edu>
2024-02-27 15:55:18 -0700

    Updated test file for saving trees and saving trained RF

--- utils/ex_iris_rf.py
+++ utils/ex_iris_rf.py
@@ -7,9 +7,11 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import export_text, plot_tree
+from joblib import dump, load
 
 
 fn_save_trees = "/data/ssd/eduardojh/results/iris_tree.txt"
+fn_trained_rf = "/data/ssd/eduardojh/results/trained_model.joblib"
 
 iris = load_iris()
 
@@ -24,14 +26,33 @@ print(iris.target)
 
 X_train, X_test, y_train, y_test = train_test_split(df.drop(['target'], axis='columns'), iris.target, test_size=0.2)
 
-clf = RandomForestClassifier(n_estimators=50)
+n_trees = 50
+print(f"Training random forest with {n_trees} estimators...")
+
+clf = RandomForestClassifier(n_estimators=n_trees)
 clf.fit(X_train, y_train)
 
+print("Saving trained model...", end='')
+dump(clf, fn_trained_rf)
+print("done!")
+
+print("Saving tree: ", end='')
 for i, tree in enumerate(clf.estimators_):
     # A text representation
-    tree_str = export_text(tree, feature_names=iris.feature_names, max_depth=10)
+    print(f"{i+1}/{n_trees}", end='')
+    print("  text representation...", end='')
+    tree_str = export_text(tree, feature_names=iris.feature_names, max_depth=len(iris.feature_names), decimals=0)
     with open(fn_save_trees[:-4] + '_' +  str(i).zfill(3) + '.txt', 'w') as f_tree:
         f_tree.write(tree_str)
     # A figure
+    print("  graphical representation...")
+    plt.figure()
     plot_tree(tree)
-    plt.savefig(fn_save_trees[:-4] + '_' +  str(i).zfill(3) + '.png', bbox_inches='tight', dpi=600)
+    plt.savefig(fn_save_trees[:-4] + '_' +  str(i).zfill(3) + '.eps', format='eps', bbox_inches='tight')
+
+print("Loading trained model...")
+trained_clf = load(fn_trained_rf)
+print("Predicting...")
+predictions = trained_clf.predict(X_test)
+
+print("All done! ;-)")
