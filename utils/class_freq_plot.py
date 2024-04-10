271247fc0c42f628523a16b22266bbc6ed05c877
eduardojh <eduardojh@arizona.edu>
2024-04-09 19:14:11 -0700

    Plots of land cover frequency and transitions

--- utils/class_freq_plot.py
+++ utils/class_freq_plot.py
@@ -6,41 +6,87 @@
 import os
 import sys
 sys.path.insert(0, '/data/ssd/eduardojh/land_cover_analysis/lib/')
+import pandas as pd
+import matplotlib.pyplot as plt
 
-import rsmodule as rs
+# import rsmodule as rs
 
+plt.style.use('ggplot')  # R-like plots
 
-fn_landcover = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2013_2016/data/usv250s5ugw_grp11_ancillary.tif"
+# fn_landcover = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2013_2016/data/usv250s5ugw_grp11_ancillary.tif"
+# fig_frequencies = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2013_2016/sampling_grp11_3M/class_fequencies_sorted.png"
+
+# # Read the land cover raster and retrive the land cover classes
+# assert os.path.isfile(fn_landcover) is True, f"ERROR: File not found! {fn_landcover}"
+# land_cover, nodata, geotransform, spatial_reference = rs.open_raster(fn_landcover)
+# print(f'  Opening raster: {fn_landcover}')
+# print(f'    --NoData        : {nodata}')
+# print(f'    --Columns       : {land_cover.shape[1]}')
+# print(f'    --Rows          : {land_cover.shape[0]}')
+# print(f'    --Geotransform  : {geotransform}')
+# print(f'    --Spatial ref.  : {spatial_reference}')
+# print(f'    --Type          : {land_cover.dtype}')
+
+# land_cover = land_cover.filled(0)
+
+# # Create a list of land cover keys and its area covered percentage
+# landcover_frequencies = rs.land_cover_freq(fn_landcover, verbose=False, sort=True)
+# print(f'  --Land cover frequencies: {landcover_frequencies}')
+
+# classes = list(landcover_frequencies.keys())
+# freqs = [landcover_frequencies[x] for x in classes]  # pixel count
+# print(classes)
+# print(freqs)
+# percentages = (freqs/sum(freqs))*100
+# print(percentages)
+
+# # Plot land cover percentage horizontal bar
+# print('  --Plotting land cover percentages...')
+# rs.plot_land_cover_hbar(classes, percentages, fig_frequencies,
+#     title='INEGI Land Cover Classes in Yucatan Peninsula',
+#     xlabel='Percentage (based on pixel count)',
+#     ylabel='Land Cover (Grouped)',  # remove if not grouped
+#     xlims=(0,60))
+
+fn_samplesize = "//VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2013_2016/sampling_grp11_3M/dataset_sample_sizes.csv"
 fig_frequencies = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/2013_2016/sampling_grp11_3M/class_fequencies_sorted.png"
 
-# Read the land cover raster and retrive the land cover classes
-assert os.path.isfile(fn_landcover) is True, f"ERROR: File not found! {fn_landcover}"
-land_cover, nodata, geotransform, spatial_reference = rs.open_raster(fn_landcover)
-print(f'  Opening raster: {fn_landcover}')
-print(f'    --NoData        : {nodata}')
-print(f'    --Columns       : {land_cover.shape[1]}')
-print(f'    --Rows          : {land_cover.shape[0]}')
-print(f'    --Geotransform  : {geotransform}')
-print(f'    --Spatial ref.  : {spatial_reference}')
-print(f'    --Type          : {land_cover.dtype}')
+df_sample = pd.read_csv(fn_samplesize)
 
-land_cover = land_cover.filled(0)
+print(df_sample)
+df_sorted = df_sample.sort_values(['Percent'])#, ascending=False)
+print(df_sorted)
 
-# Create a list of land cover keys and its area covered percentage
-landcover_frequencies = rs.land_cover_freq(fn_landcover, verbose=False, sort=True)
-print(f'  --Land cover frequencies: {landcover_frequencies}')
+# # Plot land cover percentage horizontal bar
+# print('  --Plotting land cover percentages...')
+# rs.plot_land_cover_hbar(classes, percentages, fig_frequencies,
+#     title='INEGI Land Cover Classes in Yucatan Peninsula',
+#     xlabel='Percentage (based on pixel count)',
+#     ylabel='Land Cover (Grouped)',  # remove if not grouped
+#     xlims=(0,60))
 
-classes = list(landcover_frequencies.keys())
-freqs = [landcover_frequencies[x] for x in classes]  # pixel count
-print(classes)
-print(freqs)
-percentages = (freqs/sum(freqs))*100
-print(percentages)
+labels = {101: 'Agriculture',
+          102: 'Urban',
+          103: 'Water',
+          104: 'Barren land',
+          105: 'Mangrove',
+          106: 'Evergreen tropical forest',
+          107: 'Savanna',
+          108: 'Wetland',
+          109: 'Deciduous tropical forest',
+          110: 'Coastal vegetation',
+          111: 'Oak forest'}
+x_lbls = [labels[x] for x in df_sorted['Key']]
+print(x_lbls)
 
-# Plot land cover percentage horizontal bar
-print('  --Plotting land cover percentages...')
-rs.plot_land_cover_hbar(classes, percentages, fig_frequencies,
-    title='INEGI Land Cover Classes in Yucatan Peninsula',
-    xlabel='Percentage (based on pixel count)',
-    ylabel='Land Cover (Grouped)',  # remove if not grouped
-    xlims=(0,60))
+pl = plt.barh(x_lbls, df_sorted['Percent'])
+for bar in pl:
+    value = bar.get_width()
+    text = round(value, 4)
+    if value > 0.01:
+        text = round(value, 2)
+    plt.annotate(text, xy=(value+0.1, bar.get_y()+0.25))
+plt.xlabel("Percent")
+
+plt.savefig(fig_frequencies, bbox_inches='tight', dpi=150)
+plt.close()
---
+++ utils/transitions_plot.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+""" Heatmap of the land cover transitions """
+
+import os
+import sys
+sys.path.insert(0, '/data/ssd/eduardojh/land_cover_analysis/lib/')
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# import rsmodule as rs
+
+# plt.style.use('ggplot')  # R-like plots
+
+nlabels = [x+0.5 for x in range(0,11)]
+print(nlabels)
+labels = ['Agriculture',
+          'Urban',
+          'Water',
+          'Barren land',
+          'Mangrove',
+          'Evergreen tropical forest',
+          'Savanna',
+          'Wetland',
+          'Deciduous tropical forest',
+          'Coastal vegetation',
+          'Oak forest']
+
+# For the entire Yucatan Peninsula
+# fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/land_cover_class_changes_table.csv"
+# fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/land_cover_class_changes_table_percent.csv"
+# fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/transitions_heatmap.png"
+
+# Inside the CBR
+fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_land_cover_class_changes_table.csv"
+fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_land_cover_class_changes_table_percent.csv"
+fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_transitions_heatmap.png"
+
+# Outsise the CBR
+# fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/SIMAREA_land_cover_class_changes_table.csv"
+# fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/SIMAREA_land_cover_class_changes_table_percent.csv"
+# fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/SIMAREA_transitions_heatmap.png"
+
+df_trans = pd.read_csv(fn_transitions, index_col=0)
+print(df_trans)
+print(df_trans.columns)
+
+df_percent = pd.read_csv(fn_percent, index_col=0)
+print(df_percent)
+print(df_percent.columns)
+
+dataset = {}
+for col in range(len(labels)):
+    column = []
+    for row in range(len(labels)):
+        if df_trans.iloc[row, col] < 0:
+            column.append("")
+        else:
+            column.append(f"{df_trans.iloc[row, col]:,.0f}\n{df_percent.iloc[row, col]:.1f}%")
+        # print(f"{row},{col}: {df_trans.iloc[row, col]:,.1f}\n{df_percent.iloc[row, col]:.1f}%")
+    dataset[100+col+1] = column
+# print(dataset)
+df_all = pd.DataFrame(dataset)
+print(df_all)
+
+# If CBR remove the last two classes
+df_all = df_all.drop(columns=[110, 111])
+df_all = df_all.drop([9, 10])
+df_percent = df_percent.drop(columns=['110', '111'])
+df_percent = df_percent.drop([110, 111])
+labels.pop()
+labels.pop()
+nlabels.pop()
+nlabels.pop()
+
+# Plot percentages and annotate with actual values
+# fig = plt.figure(figsize=(16, 8), constrained_layout=True)
+fig = plt.figure(figsize=(12, 8), constrained_layout=True)
+ax = sns.heatmap(df_percent, cmap='crest', annot=df_all, fmt = '', linewidths=0.5, vmin=0, vmax=100)
+# ax = sns.heatmap(df_percent, cmap='crest', annot=df_trans, fmt=",.1f", linewidths=0.5)
+# ax.xaxis.tick_top()
+ax.set_xticks(nlabels, labels, rotation=90)
+ax.set_yticks(nlabels, labels, rotation=0)
+ax.set_xlabel("Land cover classes in P1 (2013-2016)")
+ax.set_ylabel("Land cover classes in P3 (2019-2022)")
+
+
+plt.savefig(fig_heatmap, bbox_inches='tight', dpi=100)
+# plt.savefig(fig_heatmap, bbox_inches='tight', dpi=150)
+plt.close()
+
+
+# # Plot and annotate with percentages
+# fig = plt.figure(figsize=(16, 8), constrained_layout=True)
+# ax = sns.heatmap(df_percent, cmap='crest', annot=True, fmt=",.1f", linewidths=0.5)
+# ax.xaxis.tick_top()
+# ax.set_xticks(nlabels, labels, rotation=90)
+# ax.set_yticks(nlabels, labels, rotation=0)
+
+# plt.savefig(fig_heatmap[:-4] + "_percent.png", bbox_inches='tight', dpi=150)
+# plt.close()
