973d04a22f22094273cced567be67109d8abe43e
eduardojh <eduardojh@arizona.edu>
2024-06-11 15:19:36 -0700

    Changed color of plots

--- utils/transitions_plot.py
+++ utils/transitions_plot.py
@@ -29,19 +29,23 @@ labels = ['Agriculture',
           'Oak forest']
 
 # For the entire Yucatan Peninsula
-# fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/land_cover_class_changes_table.csv"
-# fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/land_cover_class_changes_table_percent.csv"
-# fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/transitions_heatmap.png"
+fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/land_cover_class_changes_table.csv"
+fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/land_cover_class_changes_table_percent.csv"
+fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/change/transitions_heatmap.png"
+cbr = False
+size = (18, 10)
 
 # Inside the CBR
-fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_land_cover_class_changes_table.csv"
-fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_land_cover_class_changes_table_percent.csv"
-fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_transitions_heatmap.png"
+# fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_land_cover_class_changes_table.csv"
+# fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_land_cover_class_changes_table_percent.csv"
+# fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/CBR_transitions_heatmap.png"
+# cbr = True
 
 # Outsise the CBR
 # fn_transitions = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/SIMAREA_land_cover_class_changes_table.csv"
 # fn_percent = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/SIMAREA_land_cover_class_changes_table_percent.csv"
 # fig_heatmap = "/VIP/engr-didan02s/DATA/EDUARDO/YUCATAN_LAND_COVER/ROI2/effectiveness/SIMAREA_transitions_heatmap.png"
+# cbr = True
 
 df_trans = pd.read_csv(fn_transitions, index_col=0)
 print(df_trans)
@@ -66,19 +70,22 @@ df_all = pd.DataFrame(dataset)
 print(df_all)
 
 # If CBR remove the last two classes
-df_all = df_all.drop(columns=[110, 111])
-df_all = df_all.drop([9, 10])
-df_percent = df_percent.drop(columns=['110', '111'])
-df_percent = df_percent.drop([110, 111])
-labels.pop()
-labels.pop()
-nlabels.pop()
-nlabels.pop()
+if cbr:
+    df_all = df_all.drop(columns=[110, 111])
+    df_all = df_all.drop([9, 10])
+    df_percent = df_percent.drop(columns=['110', '111'])
+    df_percent = df_percent.drop([110, 111])
+    labels.pop()
+    labels.pop()
+    nlabels.pop()
+    nlabels.pop()
+    size = (12, 8)
 
 # Plot percentages and annotate with actual values
 # fig = plt.figure(figsize=(16, 8), constrained_layout=True)
-fig = plt.figure(figsize=(12, 8), constrained_layout=True)
-ax = sns.heatmap(df_percent, cmap='crest', annot=df_all, fmt = '', linewidths=0.5, vmin=0, vmax=100)
+fig = plt.figure(figsize=size, constrained_layout=True)
+# Try: 'PuBuGn', 'BuGn', 'twilight', 'GnBu'. Original: 'crest'
+ax = sns.heatmap(df_percent, cmap='PuBuGn', annot=df_all, fmt = '', linewidths=0.5, vmin=0, vmax=100)
 # ax = sns.heatmap(df_percent, cmap='crest', annot=df_trans, fmt=",.1f", linewidths=0.5)
 # ax.xaxis.tick_top()
 ax.set_xticks(nlabels, labels, rotation=90)
@@ -90,14 +97,3 @@ ax.set_ylabel("Land cover classes in P3 (2019-2022)")
 plt.savefig(fig_heatmap, bbox_inches='tight', dpi=100)
 # plt.savefig(fig_heatmap, bbox_inches='tight', dpi=150)
 plt.close()
-
-
-# # Plot and annotate with percentages
-# fig = plt.figure(figsize=(16, 8), constrained_layout=True)
-# ax = sns.heatmap(df_percent, cmap='crest', annot=True, fmt=",.1f", linewidths=0.5)
-# ax.xaxis.tick_top()
-# ax.set_xticks(nlabels, labels, rotation=90)
-# ax.set_yticks(nlabels, labels, rotation=0)
-
-# plt.savefig(fig_heatmap[:-4] + "_percent.png", bbox_inches='tight', dpi=150)
-# plt.close()
